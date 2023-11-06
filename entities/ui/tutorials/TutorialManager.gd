ac1c230ddc0ac0e939254778c2ba99dcc9e5ec6a
Stephan Hillebrand <5377412+Taschi120@users.noreply.github.com>
2023-11-06 18:55:35 +0100

    dismiss tutorial with enter key

--- entities/ui/tutorials/TutorialManager.gd
+++ entities/ui/tutorials/TutorialManager.gd
@@ -4,3 +4,8 @@ class_name TutorialManager
 
 func show_basic_controls() -> void:
 	$BasicControls.visible = true
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("ui_accept"):
+		for child in get_children():
+			child.visible = false
