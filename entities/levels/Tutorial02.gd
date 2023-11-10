f47d1b007094f18deed904eeeba801dafb82c486
Stephan <5377412+Taschi120@users.noreply.github.com>
2023-11-10 20:42:22 +0100

    more work on mobs

--- entities/levels/Tutorial02.gd
+++ entities/levels/Tutorial02.gd
@@ -1,2 +1,4 @@
 extends Level
 
+func _show_tutorials() -> void:
+	tutorial_manager.show_mob_tutorial()
--- entities/ui/tutorials/TutorialManager.gd
+++ entities/ui/tutorials/TutorialManager.gd
@@ -4,8 +4,14 @@ class_name TutorialManager
 
 func show_basic_controls() -> void:
 	$BasicControls.visible = true
+	
+func show_mob_tutorial() -> void:
+	$Mobs.visible = true
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed("ui_accept"):
 		for child in get_children():
 			child.visible = false
+			
+	if event.is_action_pressed("blelele"):
+		$Mobs.visible = false
