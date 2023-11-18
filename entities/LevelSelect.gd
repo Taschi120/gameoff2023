2a2a79081dfbe26d7c5b8ac5bb792c23ed5a8565
Stephan Hillebrand <5377412+Taschi120@users.noreply.github.com>
2023-11-18 20:17:35 +0100

    made info window accessible

--- entities/LevelSelect.gd
+++ entities/LevelSelect.gd
@@ -14,3 +14,11 @@ func _ready() -> void:
 func _on_level_button_pressed(level: Dictionary) -> void:
 	print("Level chosen: %s [%s]" % [level["display_name"], level["id"]])
 	LevelManager.load_level(level, get_tree(), self)
+
+
+func _on_info_button_pressed() -> void:
+	$MainMenu.visible = true
+
+
+func _on_main_menu_closed() -> void:
+	$MainMenu.visible = false
