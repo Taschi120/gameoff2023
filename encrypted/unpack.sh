4661070a34d1598f12ed2139915e9dd214a8eee4
Stephan Hillebrand <5377412+Taschi120@users.noreply.github.com>
2023-11-19 05:27:07 +0100

    still trying to fix CI

--- encrypted/unpack.sh
+++ encrypted/unpack.sh
@@ -7,7 +7,7 @@ mkdir -p ../unencrypted/
 for FILE in *.zip.gpg; do
 	BASENAME=`basename $FILE`
 	rm -f ../unencrypted/$BASENAME.zip
-	echo $ASSET_DECRYPTION_KEY | gpg -o ../unencrypted/$BASENAME.zip --batch --pinentry-mode loopback -v -d $FILE
+	gpg -o ../unencrypted/$BASENAME.zip --batch --no-tty --passphrase $ASSET_DECRYPTION_KEY -v -d $FILE
 	cd ../unencrypted/
 	unzip -u $BASENAME.zip -d $BASENAME
 	rm $BASENAME.zip
